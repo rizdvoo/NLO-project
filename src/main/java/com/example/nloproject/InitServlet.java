9844cb05cf7f6449125e4e80307d114096af5e67
Богдан Васильєв <bogdanvasilev2017@gmail.com>
2023-11-27 19:36:07 +0200

    first

--- src/main/java/com/example/nloproject/InitServlet.java
+++ src/main/java/com/example/nloproject/InitServlet.java
@@ -8,6 +8,16 @@ import jakarta.servlet.annotation.*;
 @WebServlet(name = "init", value = "/init")
 public class InitServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HttpSession session = request.getSession(true);
+        response.setContentType("text/xml;charset=UTF-8");
 
+        String localFilePath = "C:\\Users\\byvas\\IdeaProjects\\NLO-project\\src\\main\\webapp\\jsp\\main.jsp";
+        String realPath = getServletContext().getRealPath(localFilePath);
+
+        File file = new File(realPath);
+
+        if (file.exists()) {
+            response.sendRedirect("file://" + realPath);
+        } else response.getWriter().println("File not found.");
     }
 }
